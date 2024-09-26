67c5c76edf1c65eddf94eaa7d7d30ca8ba1094d9
florence <loracodes3@gmail.com>
2024-09-26 20:40:38 +0300

    outputing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // string interpolation
 const a=5;
 const b=10;
-console.log(a)
-console.log(b)
+console.log(a) //outputs the value of a (5) upon running node index.js(the name of your file) in the terminal
+console.log(b) //outputs the value of b
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + "."); //
